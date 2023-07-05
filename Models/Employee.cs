1b0b7361dc96503a386b20f64031c08557ed2f78
Payal Padmalaya Prusty <98264541+PayalPadmalyaPrusty@users.noreply.github.com>
2023-07-05 23:52:32 +0530

    models folder

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Attendance.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public int EMP_ID { get; set; }
+        public string EMP_Name { get; set; }
+        public DateTime Attend_Date { get; set; }
+        public DateTime Local_InTime { get; set; }
+        public DateTime Local_OutTime { get; set; }
+    }
+}
