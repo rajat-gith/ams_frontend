1ccc0b6d8695f6ab08439e85845f79a6a990910b
Payal Padmalaya Prusty <98264541+PayalPadmalyaPrusty@users.noreply.github.com>
2023-07-06 14:20:21 +0530

    Controllers Folder

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Attendance.Models;
+
+namespace Attendance.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowEmployeesLessThan8Hours()
+        {
+            List<Employee> employees = GetEmployeesLessThan8Hours();
+
+            return View(employees);
+        }
+        private List<Employee> GetEmployeesLessThan8Hours()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            // Establish a connection to the database
+            using (SqlConnection connection = new SqlConnection("Data Source = LAPTOP-MRRGQ6LL\\SQLEXPRESS; Database = Attendance; Integrated Security = True"))
+            {
+                // Open the connection
+                connection.Open();
+
+                // Create a SQL query to fetch employees working less than 8 hours
+                string query = "SELECT EMP_Name" +
+                    "FROM local" +
+                    "WHERE ID IN(SELECT ID FROM local WHERE DATEDIFF(HOUR, Local_InTime, Local_OutTime) < 8)";
+
+                // Create a command object with the query and connection
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Execute the query and retrieve the results
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Read the data and map it to Employee objects
+                        while (reader.Read())
+                        {
+                            Employee employee = new Employee()
+                            {
+                                ID = Convert.ToInt32(reader["ID"]),
+                                EMP_ID = Convert.ToInt32(reader["EMP_ID"]),
+                                EMP_Name = reader["EMP_Name"].ToString(),
+                                Attend_Date = Convert.ToDateTime(reader["Attend_Date"]),
+                                Local_InTime = Convert.ToDateTime(reader["Local_InTime"]),
+                                Local_OutTime = Convert.ToDateTime(reader["Local_OutTime"])
+                            };
+
+                            employees.Add(employee);
+                        }
+                    }
+                }
+            }
+
+            return employees;
+        }
+
+    }
+}
